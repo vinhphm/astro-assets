1be91f22a39cff3cec03d51d8e07e456148da9cc
vinhphm <vinh@vinh.dev>
2023-03-28 23:59:53 +0700

    4 files modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,9 @@ export default defineConfig({
   experimental: {
     assets: true,
   },
-  image: {
-    service: 'astro/assets/services/sharp',
-  },
+  // image: {
+  //   service: 'astro/assets/services/sharp',
+  // },
   integrations: [mdx(), sitemap()],
   output: 'server',
   adapter: vercel(),
